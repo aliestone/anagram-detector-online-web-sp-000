ba9252a954ddae4836d280803a1943f8201847de
Alie Stone <helloaliestone@gmail.com>
2020-09-28 17:33:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ attr_accessor :word
   def match(array)
     matches = []
     array.each.do |word|
-    matches << word if word.split('').sort == self.word.split('').sort 
+    matches << word if word.split('').sort == self.word.split('').sort
     end
     matches
   end
